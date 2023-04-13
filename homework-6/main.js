3eedbabeec31323c0215bb6bef5cffa95d95945f
Arseny Nikolaev <arseny.nikolaev@bk.ru>
2023-04-13 14:03:23 +0300

    homework-6 fixed;

--- homework-6/main.js
+++ homework-6/main.js
@@ -1,13 +1,12 @@
 "use strict";
 
-function loadJSON(callback) {
+function loadJSON(filePath, callback) {
     var XMLObj = new XMLHttpRequest();
-    XMLObj.open('GET', 'data.json', true);
+    XMLObj.open('GET', filePath, true);
     XMLObj.onreadystatechange = function () {
         if (XMLObj.readyState === 4 && XMLObj.status === 200) {
             var myArr = JSON.parse(this.responseText);
             callback(myArr);
-            a1 = myArr;
         }
     };
     XMLObj.send();
@@ -18,44 +17,40 @@ function processJSON(bigJSON){
 
     function getAgesRecursive(person){
 
-        if(!person.friends){
-            if(person.age){
-                return {num:1, age: person.age};
+        if(!person.friends){//if person has no friends..
+            if(person.age){//and person has age..
+                return {num:1, age: person.age};//return person's age
             }
-            else{
+            else{//else, return zeroes
                 return {num:0, age: 0};
             }
         }
-        else{
-            if(person.isCore){   
+        else{//if person has friends..
+            if(person.isCore){   //if it's the core element, set it's age to zero and don't count it as a person
                 var friendsSummary = {num:0, age:0};
             }
-            else{
+            else{//otherwise, count person's age and add 1 to people count
                 var friendsSummary = {num:1, age:person.age};
             }
-            for(var friend of person.friends){
-                
+            for(var friend of person.friends){//then add friends amount and total age
                 var friendParams = getAgesRecursive(friend);
                 friendsSummary.num += friendParams.num;
                 friendsSummary.age += friendParams.age;
-                console.log(friend);
-                console.log(friendParams);
-                console.log(friendsSummary);
             }
-            return friendsSummary;
+            return friendsSummary;//return sum of person's age and age of his friends along with people count
         }
 
 
     }
 
-    var fixedBigJSON = {friends:bigJSON, name:"core", age:0, isCore:1};
+    var fixedBigJSON = {friends:bigJSON, name:"core", age:0, isCore:1};//creating the core element
 
-    var totals = getAgesRecursive(fixedBigJSON);
+    var totals = getAgesRecursive(fixedBigJSON);//counting total amount of people and total age
 
-    console.log(totals.age / totals.num);
+    console.log(totals.age / totals.num);//count medium age of all people
 
 
 }
 
 
-loadJSON(processJSON);
+loadJSON("data.json", processJSON);//execute the function using data from data.json
